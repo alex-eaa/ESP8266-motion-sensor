15d1808f43d55db0b28310abf3c1ea82e5d825c1
alex-eaa <elchaninov79@gmail.com>
2020-05-13 15:18:25 +0300

    add functions file

---
+++ functions.ino
@@ -0,0 +1,32 @@
+//Print configuration parameters
+void printConfiguration () {
+  Serial.println(F("Print ALL VARIABLE:"));
+  Serial.print(F("wifiAP_mode="));  Serial.println(wifiAP_mode);
+  Serial.print(F("p_ssidAP="));     Serial.println(p_ssidAP);
+  Serial.print(F("p_passwordAP=")); Serial.println(p_passwordAP);
+  Serial.print(F("p_ssid="));       Serial.println(p_ssid);
+  Serial.print(F("p_password="));   Serial.println(p_password);
+  Serial.print(F("static_IP="));     Serial.println(static_IP);
+  Serial.print(F("ip="));    Serial.print(ip[0]);   Serial.print(":");  Serial.print(ip[1]);  Serial.print(":");  Serial.print(ip[2]);  Serial.print(":");  Serial.println(ip[3]);
+  Serial.print(F("sbnt="));  Serial.print(sbnt[0]); Serial.print(":");  Serial.print(sbnt[1]);  Serial.print(":");  Serial.print(sbnt[2]);  Serial.print(":");  Serial.println(sbnt[3]);
+  Serial.print(F("gtw="));   Serial.print(gtw[0]);  Serial.print(":");  Serial.print(gtw[1]);  Serial.print(":");  Serial.print(gtw[2]);  Serial.print(":");  Serial.println(gtw[3]);
+}
+
+
+
+
+void printChipInfo() {
+  Serial.print(F("\n<-> LAST RESET REASON: "));  Serial.println(ESP.getResetReason());
+  Serial.print(F("<-> ESP8266 CHIP ID: "));      Serial.println(String(ESP.getChipId(), HEX));
+  Serial.print(F("<-> CORE VERSION: "));         Serial.println(ESP.getCoreVersion());
+  Serial.print(F("<-> SDK VERSION: "));          Serial.println(ESP.getSdkVersion());
+  Serial.print(F("<-> CPU FREQ MHz: "));         Serial.println(ESP.getCpuFreqMHz());
+  Serial.print(F("<-> FLASH CHIP ID: "));        Serial.println(String(ESP.getFlashChipId(), HEX));
+  Serial.print(F("<-> FLASH CHIP SIZE: "));      Serial.println(ESP.getFlashChipSize());
+  Serial.print(F("<-> FLASH CHIP REAL SIZE: ")); Serial.println(ESP.getFlashChipRealSize());
+  Serial.print(F("<-> FLASH CHIP SPEED: "));     Serial.println(ESP.getFlashChipSpeed());
+  Serial.print(F("<-> FREE MEMORY: "));          Serial.println(ESP.getFreeHeap());
+  Serial.print(F("<-> SKETCH SIZE: "));          Serial.println(ESP.getSketchSize());
+  Serial.print(F("<-> FREE SKETCH SIZE: "));     Serial.println(ESP.getFreeSketchSpace());
+  Serial.print(F("<-> CYCLE COUNTD: "));         Serial.println(ESP.getCycleCount());
+}
